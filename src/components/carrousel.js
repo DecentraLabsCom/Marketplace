bf2f371fb380f19e5e81eaa0af6181ebf9ecd3bc
Sonia-E <soniaenjutom94@gmail.com>
2025-03-28 12:32:40 +0100

    Added light blue color to highlight current image slide

--- src/components/carrousel.js
+++ src/components/carrousel.js
@@ -63,7 +63,7 @@ export default function Carrousel({ lab }) {
             className={`mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] 
               border-solid border-transparent bg-white hover:bg-blue-500 bg-clip-padding p-0 -indent-[999px] opacity-50 
               transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none ${
-              index === currentIndex ? 'opacity-100' : ''
+              index === currentIndex ? 'opacity-100 bg-blue-300' : ''
             }`}
             aria-current={index === currentIndex ? 'true' : 'false'}
             aria-label={`Slide ${index + 1}`}
