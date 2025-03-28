a832287a347a23aa77446c9deeb732c6e9c8a421
Sonia-E <soniaenjutom94@gmail.com>
2025-03-28 11:28:10 +0100

    Specified which image to show now that there are more than one per lab

--- src/components/LabCard.js
+++ src/components/LabCard.js
@@ -40,7 +40,8 @@ export default function LabCard({ id, name, provider, description, price, auth, 
       ${hasActiveBooking ? 'border-4 border-[#715c8c] animate-glow' : ''}`} 
       style={{ height: '400px' }}>
       <div className="h-2/3">
-        <img src={image} alt={name} className="w-full h-full object-cover rounded-t-md" />
+        {/* Only show first image of each lab */}
+        <img src={image[0]} alt={name} className="w-full h-full object-cover rounded-t-md" />
       </div>
       <div className="p-4 h-1/3">
         <h2 className="text-2xl font-bold mt-4 text-[#333f63]">{name}</h2>
