808c90f5c4c3199ee170d2117107472cd019bec5
Sonia-E <soniaenjutom94@gmail.com>
2025-03-31 09:42:01 +0200

    In the middle of modifying LabDetailPage appearance

--- src/pages/LabDetailPage.js
+++ src/pages/LabDetailPage.js
@@ -46,18 +46,22 @@ export default function LabDetailPage({ id }) {
   }
 
   return (
-    <div className="container mx-auto p-10">
-      <Carrousel lab={lab} />
-      <h2 className="text-lg font-bold mt-2">{lab.name}</h2>
-      <p className="text-gray-400 text-sm text-justify">{lab.description}</p>
-      <p className="text-blue-600 font-semibold mt-2">{lab.price} ETH</p>
-      <button
-        onClick={handleRent}
-        disabled={isLoading}
-        className="bg-green-600 text-white px-4 py-2 rounded mt-3 w-full disabled:opacity-50"
-      >
-        {isLoading ? "Renting..." : "Rent Lab"}
-      </button>
+    <div className=" container mx-auto p-10">
+      <div className='flex flex-col md:flex-row'>
+      <div className='md:w-4/5 ' ><Carrousel lab={lab} /></div>
+      <div className="md:w-2/5 md:ml-4">
+        <h2 className="text-lg font-bold mt-2">{lab.name}</h2>
+        <p className="text-gray-400 text-sm text-justify">{lab.description}</p>
+        <p className="text-blue-600 font-semibold mt-2">{lab.price} ETH</p>
+        <button
+          onClick={handleRent}
+          disabled={isLoading}
+          className="bg-green-600 text-white px-4 py-2 rounded mt-3 w-full disabled:opacity-50"
+        >
+          {isLoading ? "Renting..." : "Rent Lab"}
+        </button>
+      </div>
+      </div>
     </div>
   )
 }
