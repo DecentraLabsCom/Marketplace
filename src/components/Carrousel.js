f1e73f4492183f2f07a6f0e60e3f736d4e32b7f6
Luis de la Torre <ldelatorre@dia.uned.es>
2025-04-10 19:27:10 +0200

    Making handlers and arrows in the images carrousel only visible when more than one image is available.
    Removing unused imports in DocsCarrousel

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -47,51 +47,55 @@ export default function Carrousel({ lab, maxHeight }) {
           </div>
         ))}
 
-      {/* Slide handles */}
-      <div className="absolute inset-x-0 bottom-0 z-[20] mx-[15%] mb-4 flex list-none justify-center p-0 pointer-events-auto">
-      {lab.image.map((_, index) => (
-        <button key={index} type="button" data-twe-slide-to={index}
-          className={`mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] 
-          border-solid border-transparent bg-white hover:bg-blue-500 bg-clip-padding p-0 -indent-[999px] opacity-50 
-          transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none 
-          ${ index === currentIndex ? 'opacity-100 bg-blue-400' : '' }`}
-          aria-current={index === currentIndex ? 'true' : 'false'} aria-label={`Slide ${index + 1}`}
-          onClick={() => handleSlide(index)} />
-      ))}
-      </div>
+      {lab?.image.length > 1 && (
+      <>
+        {/* Slide handles */}
+        <div className="absolute inset-x-0 bottom-0 z-[20] mx-[15%] mb-4 flex list-none justify-center p-0 pointer-events-auto">
+        {lab.image.map((_, index) => (
+          <button key={index} type="button" data-twe-slide-to={index}
+            className={`mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] 
+            border-solid border-transparent bg-white hover:bg-blue-500 bg-clip-padding p-0 -indent-[999px] opacity-50 
+            transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none 
+            ${ index === currentIndex ? 'opacity-100 bg-blue-400' : '' }`}
+            aria-current={index === currentIndex ? 'true' : 'false'} aria-label={`Slide ${index + 1}`}
+            onClick={() => handleSlide(index)} />
+        ))}
+        </div>
 
-      {/* Arrow handles */}
-      {/* Move to previous image */}
-      <button onClick={handlePrev} className="absolute bottom-0 left-0 top-0 z-[20] flex w-[15%] items-center 
-        justify-center border-0 bg-none p-0 text-white opacity-100 hover:opacity-90 duration-150 
-        transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] pointer-events-auto group" type="button">
-        <span className="inline-block h-8 w-8">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" 
+        {/* Arrow handles */}
+        {/* Move to previous image */}
+        <button onClick={handlePrev} className="absolute bottom-0 left-0 top-0 z-[20] flex w-[15%] items-center 
+          justify-center border-0 bg-none p-0 text-white opacity-100 hover:opacity-90 duration-150 
+          transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] pointer-events-auto group" type="button">
+          <span className="inline-block h-8 w-8">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" 
+              stroke="currentColor" className="h-6 w-6 transition-colors duration-150 ease-in-out 
+              group-hover:text-blue-500">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 
+            ![clip:rect(0,0,0,0)]">
+            Previous
+          </span>
+        </button>
+
+        {/* Move to next image */}
+        <button onClick={handleNext} className="absolute bottom-0 right-0 top-0 z-[20] flex w-[15%] items-center 
+          justify-center border-0 bg-none p-0 text-white opacity-100 hover:opacity-90 duration-150 
+          transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] pointer-events-auto group" type="button">
+          <span className="inline-block h-8 w-8">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" 
             stroke="currentColor" className="h-6 w-6 transition-colors duration-150 ease-in-out 
-            group-hover:text-blue-500">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-        </span>
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 
-          ![clip:rect(0,0,0,0)]">
-          Previous
-        </span>
-      </button>
-
-      {/* Move to next image */}
-      <button onClick={handleNext} className="absolute bottom-0 right-0 top-0 z-[20] flex w-[15%] items-center 
-        justify-center border-0 bg-none p-0 text-white opacity-100 hover:opacity-90 duration-150 
-        transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] pointer-events-auto group" type="button">
-        <span className="inline-block h-8 w-8">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" 
-          stroke="currentColor" className="h-6 w-6 transition-colors duration-150 ease-in-out 
-            group-hover:text-blue-500">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </span>
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 
-            ![clip:rect(0,0,0,0)]">Next</span>
-      </button>
+              group-hover:text-blue-500">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 
+              ![clip:rect(0,0,0,0)]">Next</span>
+        </button>
+      </>
+      )}
     </div>
   );
 }
--- src/components/DocsCarrousel.js
+++ src/components/DocsCarrousel.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 
 export default function DocsCarrousel({ docs, maxHeight }) {
   const [currentIndex, setCurrentIndex] = useState(0);
