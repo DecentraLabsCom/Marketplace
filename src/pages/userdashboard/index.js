0d8aa98f47c5370d3186329ad1dbbe0e632ace52
Sonia-E <soniaenjutom94@gmail.com>
2025-04-22 13:18:36 +0200

    User dashboard: Added access availability dates @close #39 , remove set active and all related logic for testing @close #40 @close #45 and presented the booked lab info for the active lab directly @close #46

--- src/pages/userdashboard/index.js
+++ src/pages/userdashboard/index.js
@@ -19,6 +19,9 @@ export default function UserDashboard() {
     (lab) => Array.isArray(lab.bookingInfo) && lab.bookingInfo.some(b => b.activeBooking)
   );
   const [firstActiveLab, setFirstActiveLab] = useState(null);
+  const [availableLab, setAvailableLab] = useState(null);
+  const [labDate, setLabDate] = useState(null);
+  const currentDate = new Date().toISOString().slice(0, 10);
 
   useEffect(() => {
     if (labs && labs.length > 0) {
@@ -58,36 +61,37 @@ export default function UserDashboard() {
 
   useEffect(() => {
     if (userData?.labs) {
-      const activeLab = userData.labs.find((lab) => lab.activeStatus === true);
+      const activeLab = userData.labs.find((lab) => {
+        if (lab.activeStatus === true && Array.isArray(lab.bookingInfo)) {
+          return lab.bookingInfo.some(booking => booking.date !== currentDate);
+        }
+        return false;
+      });
       setFirstActiveLab(activeLab);
+  
+      if (activeLab && Array.isArray(activeLab.bookingInfo)) {
+        const activeBooking = activeLab.bookingInfo.find(b => b.activeBooking);
+        if (activeBooking && activeBooking.date) {
+          setLabDate(activeBooking.date);
+        } else {
+          setLabDate(null);
+        }
+      }
     }
   }, [userData?.labs]);
 
-  const setActiveStatus = (labId) => {
-    setEnrichedLabs((prevLabs) =>
-      prevLabs.map((lab) => {
-        if (lab.id === labId) {
-          return { ...lab, activeStatus: true };
+  // Create useEffect to check for today's availability
+  useEffect(() => {
+    if (userData?.labs) {
+      const availableTodayLab = userData.labs.find((lab) => {
+        if (Array.isArray(lab.bookingInfo)) {
+          return lab.bookingInfo.some(booking => booking.date === currentDate);
         }
-        return lab;
-      })
-    );
-    setStatusChange((prev) => !prev);
-  };
-
-  const setInactiveStatus = (labId) => {
-    setEnrichedLabs((prevLabs) =>
-      prevLabs.map((lab) => {
-        if (lab.id === labId) {
-          return { ...lab, 
-            formerlyBooked: true,
-            activeStatus: false };
-        }
-        return lab;
-      })
-    );
-    setStatusChange((prev) => !prev);
-  };
+        return false;
+      });
+      setAvailableLab(availableTodayLab);
+    }
+  }, [userData?.labs]);
 
   if (!userData) {
     return <div className="text-center p-2">Loading user data...</div>
@@ -111,34 +115,62 @@ export default function UserDashboard() {
           <div className='flex flex-row'>
             <div className="border shadow text-white rounded p-6 mb-1 mr-1 w-2/3">
               <div className="flex flex-col">
-                {firstActiveLab && (
-                  <h2 className="text-2xl font-semibold mb-4 text-white text-center">{firstActiveLab.name}</h2>
+                {availableLab ? (
+                    <h2 className="text-2xl font-semibold mb-4 text-white text-center">Currently active: {availableLab.name}</h2>
+                ) : firstActiveLab && (
+                  <h2 className="text-2xl font-semibold mb-4 text-white text-center">Soon to be active: {firstActiveLab.name}</h2>
                 )}
                 <div className='flex flex-row flex-wrap'>
-                  {firstActiveLab && (
+                  {availableLab ? (
+                    <React.Fragment key={availableLab.id}>
+                      <div className='flex flex-col items-center'>
+                        <div key={availableLab.id} className={`w-[250px] group justify-between items-center shadow-md bg-gray-200 transform
+                          transition-transform duration-300 hover:scale-105 mr-3
+                          mb-4 border-2 p-2 h-[250px] rounded-lg flex flex-col ${availableLab.activeStatus ? 'border-4 border-[#715c8c] animate-glow' : ''}`}
+                        >
+                          <div className='rounded-lg h-[150px] w-full'>
+                            <Carrousel lab={availableLab} maxHeight={140} />
+                          </div>
+                          <span className="text-gray-700 block">Available today</span>
+                          <LabAccess userWallet={address} hasActiveBooking={availableLab.activeStatus} auth={availableLab.auth} />
+                        </div>
+                      </div>
+                      <div className={`w-5/5 ${availableLab.docs.length > 0 ? `` : 'h-[100px]'} flex-1 mb-4 flex flex-col justify-center p-2 text-center rounded-lg shadow-md bg-gray-300`}>
+                        {availableLab.docs && availableLab.docs.length > 0 && (
+                          <div key={0} className="mt-1">
+                            <iframe src={availableLab.docs[0]} title="description" height="260px" width="100%" className='rounded-lg'></iframe>
+                          </div>
+                        )}
+                        {availableLab.docs.length === 0 && (
+                          <span className="text-gray-700 text-center">No documents available</span>
+                        )}
+                        <Link href={`/lab/${availableLab.id}`} className='px-3 mt-3 py-1 rounded
+                          text-sm bg-yellow-500 hover:bg-yellow-300 text-white'>Explore this lab</Link>
+                      </div>
+                    </React.Fragment>
+                  ) : firstActiveLab && (
                     <React.Fragment key={firstActiveLab.id}>
                       <div className='flex flex-col items-center'>
                         <div key={firstActiveLab.id} className={`w-[250px] group justify-between items-center shadow-md bg-gray-200 transform
-                            transition-transform duration-300 hover:scale-105 mr-3
-                            mb-4 border-2 p-2 h-[250px] rounded-lg flex flex-col ${hasActiveBooking ? 'border-4 border-[#715c8c] animate-glow' : ''}`}
+                          transition-transform duration-300 hover:scale-105 mr-3
+                          mb-4 border-2 p-2 h-[250px] rounded-lg flex flex-col`}
                         >
                           <div className='rounded-lg h-[150px] w-full'>
                             <Carrousel lab={firstActiveLab} maxHeight={140} />
                           </div>
-                          <span className="text-gray-700 block">Available until [date]</span>
-                          <LabAccess userWallet={address} hasActiveBooking={hasActiveBooking} auth={firstActiveLab.auth} />
+                          <span className="text-gray-700 block mb-6">Available on: {labDate}</span>
                         </div>
                       </div>
                       <div className={`w-5/5 ${firstActiveLab.docs.length > 0 ? `` : 'h-[100px]'} flex-1 mb-4 flex flex-col justify-center p-2 text-center rounded-lg shadow-md bg-gray-300`}>
-                          {firstActiveLab.docs && firstActiveLab.docs.length > 0 && (
-                            <div key={0} className="mt-1">
-                              <iframe src={firstActiveLab.docs[0]} title="description" height="260px" width="100%" className='rounded-lg'></iframe>
-                            </div>
-                          )}
-                          {firstActiveLab.docs.length === 0 && (
-                            <span className="text-gray-700 text-center">No documents available</span>
-                          )}
-                          <Link href={`/lab/${firstActiveLab.id}`} className='px-3 mt-3 py-1 rounded 
+                        {firstActiveLab.docs && firstActiveLab.docs.length > 0 && (
+                          <div key={0} className="mt-1">
+                            <iframe src={firstActiveLab.docs[0]} title="description" height="260px" width="100%" className='rounded-lg'></iframe>
+                          </div>
+                        )}
+                        {firstActiveLab.docs.length === 0 && (
+                          <span className="text-gray-700 text-center">No documents available</span>
+                        )}
+                        <Link href={`/lab/${firstActiveLab.id}`} className='px-3 mt-3 py-1 rounded
                           text-sm bg-yellow-500 hover:bg-yellow-300 text-white'>Explore this lab</Link>
                       </div>
                     </React.Fragment>
@@ -146,7 +178,7 @@ export default function UserDashboard() {
                   
                 </div> 
                 {!firstActiveLab && (
-                  <span className="text-gray-300 text-center">No lab currently active</span>
+                  <span className="text-gray-300 text-center">No upcoming or currently active lab</span>
                 )}
               </div>
             </div>
@@ -161,38 +193,22 @@ export default function UserDashboard() {
           <div className="border shadow text-white rounded p-6 flex-1">
             <div className="flex flex-row gap-4">
               {/* Booked labs: active and non-active */}
-              <div className="w-1/2">
+              <div className="w-1/2 flex items-center justify-center flex-col">
                 <h2 className="text-2xl font-semibold mb-4 text-center">Upcoming Booked Labs</h2>
-                <ul className='flex justify-center'>
+                <ul className='w-4/4'>
                 {userData.labs
                   .filter(lab => Array.isArray(lab.bookingInfo) && lab.bookingInfo.some(b => b.activeBooking))
                   .map((lab) => (
                     <div className='mb-4 p-2 rounded-lg text-center'>
-                      <li key={lab.id} className="flex flex-col items-center w-full"> {/* Apilamos verticalmente */}
-                        <div className="flex items-center w-full"> {/* Línea para nombre y refund/estado */}
-                          <Link className="border-2 border-white bg-white text-black p-2 px-8 rounded-lg text-center hover:bg-slate-500 flex-grow" href={`/lab/${lab.id}`}>
-                            <span className="text-left">{lab.name}</span>
-                          </Link>
-                          <div className='mx-1'><Refund /></div> {/* Icono de reembolso */}
-                          <span className={`text-right px-3 py-1 rounded-full text-sm ${lab.activeStatus === true ? "bg-green-500 text-white" : "bg-red-500 text-white"}`}>
-                            {lab.activeStatus === true ? "Active" : "Inactive"} {/* Estado */}
-                          </span>
-                        </div>
-                        {/* Button for lab's activeStatus tests: remove this when booking system is finished */}
-                        <div className="mx-auto mt-2">
-                          {lab.activeStatus ? (
-                            <button 
-                              className='z-50 text-black text-sm border rounded-lg p-1 mr-3 bg-orange-100'
-                              onClick={() => setInactiveStatus(lab.id)}>
-                              Set Inactive
-                            </button>
-                            ) : (
-                            <button 
-                              className='z-50 text-black text-sm border rounded-lg p-1 mr-3 bg-orange-100'
-                              onClick={() => setActiveStatus(lab.id)}>
-                              Set Active
-                            </button>
-                          )}
+                      <li key={lab.id} className="flex flex-col items-center">
+                        <div className="border flex items-center w-full">
+                          <div className='border flex flex-col w-3/4'>
+                            <Link className="border-2 border-white bg-white text-black p-2 px-8 text-center hover:bg-slate-500 flex-grow" href={`/lab/${lab.id}`}>
+                              <span className="text-left">{lab.name}</span>
+                            </Link>
+                            <span>{lab.bookingInfo.find(b => b.activeBooking)?.date}</span>
+                          </div>
+                          <div className='mx-4'><Refund /></div>
                         </div>
                       </li>
                     </div>
@@ -202,20 +218,19 @@ export default function UserDashboard() {
 
                 {/* Vertical divider */}  
                 <div class="mt-1 mx-3 w-px self-stretch bg-gradient-to-tr
-              from-transparent via-neutral-800 to-transparent opacity-90 dark:via-neutral-200
-              border-l-1 border-neutral-800 dark:border-neutral-200 border-dashed"
+                    from-transparent via-neutral-800 to-transparent opacity-90 dark:via-neutral-200
+                    border-l-1 border-neutral-800 dark:border-neutral-200 border-dashed"
                     style={{ borderWidth: '4px', borderLeftStyle: 'dashed' }}>
                 </div>
-
               
-                {/* Previously booked labs */}
+                {/* Formerly booked labs */}
                 <div className="flex-1">
-                  <h2 className="text-2xl  font-semibold mb-4 text-center">Previously booked</h2>
+                  <h2 className="text-2xl  font-semibold mb-4 text-center">Formerly booked</h2>
                   <ul className='flex items-center flex-col justify-center'>
                     {userData.labs
                     .filter((lab) => lab.formerlyBooked === true)
                     .map((lab) => (
-                      <Link className="border-2 border-white bg-white text-black mb-4 p-2 px-8 rounded-lg text-center hover:bg-slate-500" href={`/lab/${lab.id}`}>
+                      <Link className="border-2 border-white bg-white text-black mb-4 p-2 px-8 text-center hover:bg-slate-500" href={`/lab/${lab.id}`}>
                         <li key={lab.id} >
                           <span>{lab.name}</span>
                         </li>
