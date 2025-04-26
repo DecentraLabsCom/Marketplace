1e5a3eb09d3055efe1a03471d32286fb280d6cd7
Luis de la Torre <ldelatorre@dia.uned.es>
2025-04-26 22:20:55 +0200

    More changes to fit ESM

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+import path from 'path';
 const appendPath = process.env.NODE_ENV === 'production' ? '/marketplace' : '';
 
 /** @type {import('next').NextConfig} */
@@ -15,4 +15,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
