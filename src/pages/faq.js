6ba0b09744fd3ca85d6d1e821c0a3fa03ffa83a7
Luis de la Torre <ldelatorre@dia.uned.es>
2025-04-27 09:58:12 +0200

    Solved eslint error for non-camelCase attribute in SVG

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -74,11 +74,11 @@ export default function FAQ() {
                 text-2xl font-semibold mb-4 text-gray-800">
                     <span>{item.question}</span>
                     <span className="transition group-open:rotate-180">
-                        <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                            stroke="black" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                            <path d="M6 9l6 6 6-6"></path>
-                        </svg>
+                    <svg fill="none" height="24" shapeRendering="geometricPrecision"
+                        stroke="black" strokeLinecap="round" strokeLinejoin="round"
+                        strokeWidth="1.5" viewBox="0 0 24 24" width="24">
+                      <path d="M6 9l6 6 6-6"></path>
+                    </svg>
                     </span>
                 </summary>
                 <p className="group-open:animate-fadeIn mt-3 text-neutral-600 text-justify">
