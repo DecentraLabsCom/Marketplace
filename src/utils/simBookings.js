c073b23ef65f70c2acef6fb318224a642aa891ab
Luis de la Torre <ldelatorre@dia.uned.es>
2025-06-02 14:31:56 +0200

    Changing simulated bookings to make them all upcoming and none in the past

--- src/utils/simBookings.js
+++ src/utils/simBookings.js
@@ -1,9 +1,9 @@
 let bookings = [
-  { labId: 23, date: "2025-05-24", time: "09:00", minutes: "60" },
-  { labId: 24, date: "2025-04-23", time: "15:30", minutes: "60" },
-  { labId: 55, date: "2025-06-12", time: "12:30", minutes: "30" },
-  { labId: 53, date: "2025-03-07", time: "10:00", minutes: "120" },
-  { labId: 56, date: "2025-07-10", time: "12:00", minutes: "30" },
+  { labId: 23, date: "2025-06-24", time: "09:00", minutes: "60" },
+  { labId: 24, date: "2025-07-23", time: "15:30", minutes: "60" },
+  { labId: 53, date: "2025-08-07", time: "18:00", minutes: "120" },
+  { labId: 55, date: "2025-06-12", time: "17:30", minutes: "30" },
+  { labId: 56, date: "2025-09-10", time: "12:00", minutes: "30" },
 ];
 
 export const simBookings = () => {
