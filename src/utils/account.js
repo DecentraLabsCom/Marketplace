ffeb15733475488d90bce0e16f4c859e2e899631
Luis de la Torre <ldelatorre@dia.uned.es>
2025-06-03 13:44:18 +0200

    SSO in production (IV)

--- src/utils/account.js
+++ src/utils/account.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 function formatAddress(address) {
+  if (!address) return "Not connected";
   return `${address.slice(0, 6)}...${address.slice(-4)}`
 }
 
@@ -30,7 +31,7 @@ export default function Account() {
     <div className="flex items-center space-x-6 ml-auto font-bold">
       {isLoggedIn && (
         <div className="pointer-events-none flex flex-col items-center">
-          <div className="text-sm text-[#333f63]">{formatAddress(address) || user?.email}</div>
+          <div className="text-sm text-[#333f63]">{user?.email || formatAddress(address)}</div>
           {(user?.name || ensName) && (
             <div className="text-[14px] text-[#335763]">
               {user?.name ? user.name : ensName }
