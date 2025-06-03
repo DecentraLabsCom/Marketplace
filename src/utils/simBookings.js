e6c3fd8965c890ba512bbc7dbe26500513c90ad5
Luis de la Torre <ldelatorre@dia.uned.es>
2025-06-03 12:24:31 +0200

    Additional change in bookings to make them all be in the future.

--- src/utils/simBookings.js
+++ src/utils/simBookings.js
@@ -1,8 +1,8 @@
 let bookings = [
-  { labId: 23, date: "2025-05-24", time: "09:00", minutes: "60" },
+  { labId: 23, date: "2025-08-24", time: "09:00", minutes: "60" },
   { labId: 24, date: "2025-06-23", time: "15:30", minutes: "60" },
   { labId: 24, date: "2025-06-23", time: "11:30", minutes: "60" },
-  { labId: 55, date: "2025-06-12", time: "12:30", minutes: "30" },
+  { labId: 55, date: "2025-07-12", time: "12:30", minutes: "30" },
   { labId: 53, date: "2025-06-07", time: "10:00", minutes: "120" },
   { labId: 56, date: "2025-07-10", time: "12:00", minutes: "30" },
 ];
