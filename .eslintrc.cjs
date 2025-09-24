7f268be371367c30393fc3b5bcac125b51bba157
andresparadaprieto <andres.parada.18@gmail.com>
2025-09-24 22:44:20 +0200

    update .eslintrc.cjs to add Jest plugin and disable react/display-name in test files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,8 @@ module.exports = {
   plugins: [
     "react",
     "react-hooks",
-    "tailwindcss"
+    "tailwindcss",
+    "jest"
   ],
   rules: {
     "react/react-in-jsx-scope": "off",
@@ -18,16 +19,30 @@ module.exports = {
     "no-unused-vars": "warn",
     "no-unreachable": "warn",
     "tailwindcss/no-custom-classname": "warn",
-    "tailwindcss/classnames-order": "off",
+    "tailwindcss/classnames-order": "off"
   },
   env: {
     browser: true,
     node: true,
-    es2021: true
+    es2021: true,
+    jest: true
   },
   settings: {
     react: {
       version: "detect"
     }
-  }
+  },
+  // Test files: disable displayName and apply Jest rules
+  overrides: [
+  {
+      files: ["**/*.test.{js,jsx,ts,tsx}"],
+       rules: {
+      "react/display-name": "off",
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/valid-expect": "error"
+              }
+   }
+  ]
 };
