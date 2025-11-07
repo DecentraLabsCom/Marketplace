a18d165fe62a55fdf91237524cd8bfa630d0de92
andresparadaprieto <andres.parada.18@gmail.com>
2025-11-08 00:28:18 +0100

    Add pre-commit and coverage report scripts

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,3 @@
-// import nextJest from 'next/jest.js';
-
-// const createJestConfig = nextJest({
-//   dir: './',
-// });
-
-// /** @type {import('jest').Config} */
-// const customJestConfig = {
-//   testEnvironment: 'jsdom',
-//   setupFilesAfterEnv: ['<rootDir>/src/test-utils/setupTests.js'],
-//   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-//   moduleNameMapper: {
-//     '^@/(.*)$': '<rootDir>/src/$1',
-//   },
-//   collectCoverageFrom: [
-//     'src/**/*.{js,jsx}',
-//     '!src/**/*.stories.{js,jsx}',
-//     '!src/test-utils/**',
-//   ],
-//   testMatch: [
-//     '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
-//     '<rootDir>/src/**/*.{test,spec}.{js,jsx}',
-//   ],
-
-//   transformIgnorePatterns: [
-//      // Allow transformation of ESM packages that Jest can't handle directly
-//   '/node_modules/(?!(wagmi|viem|next)/)',
-//   ],
-// };
-
-// export default createJestConfig(customJestConfig);
-
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
