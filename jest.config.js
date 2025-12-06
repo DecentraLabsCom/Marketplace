bb30102b216ab0064bf04f422ed3bd213185e7c5
Ravenink <ldelatorre@dia.uned.es>
2025-12-06 21:38:18 +0100

    Added ignores to jest

--- jest.config.js
+++ jest.config.js
@@ -21,6 +21,11 @@ const customJestConfig = {
     "!src/**/*.spec.{js,jsx}",
     "!src/test-utils/**",
     "!src/contracts/**", // generated ABI bundles, not part of app logic
+    "!src/utils/blockchain/wagmiConfig.js", // environment-specific config wrapper
+    "!src/utils/blockchain/networkConfig.js", // environment mapping constants
+    "!src/utils/dev/**", // dev-only helpers and fixture data
+    "!src/utils/intents/**", // experimental intent helpers not exercised in CI
+    "!src/utils/webauthn/**", // webauthn client/server glue not run in Jest
     "!src/**/*.config.js",
     "!src/app/**/layout.js",
     "!src/app/**/page.js",
