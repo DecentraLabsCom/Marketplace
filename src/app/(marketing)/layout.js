e3887798bff79142980e228796575c8fad6c5fc7
Ravenink <ldelatorre@dia.uned.es>
2026-02-19 21:23:19 +0100

    Fixes navbar elements not appearing in About, FAQ and Contact pages

--- src/app/(marketing)/layout.js
+++ src/app/(marketing)/layout.js
@@ -1,21 +1,11 @@
 import PropTypes from 'prop-types'
-import Navbar from '@/components/layout/Navbar'
-import Footer from '@/components/layout/Footer'
-import ClientOnly from '@/components/layout/ClientOnly'
+import AppProviders from '@/components/layout/AppProviders'
 
 export default function MarketingLayout({ children }) {
   return (
-    <>
-      <header className="sticky top-0 z-50">
-        <ClientOnly fallback={<div className="bg-header-bg text-hover-dark p-3 shadow-md h-20" />}>
-          <Navbar />
-        </ClientOnly>
-      </header>
-      <main className="grow">
-        {children}
-      </main>
-      <Footer />
-    </>
+    <AppProviders>
+      {children}
+    </AppProviders>
   )
 }
 
