b44beabaedf42767d9ed2a66882ca5825f291d83
Ravenink <ldelatorre@dia.uned.es>
2026-04-21 19:49:17 +0200

    Revert "Removed sessionId as a valid puc option"
    This reverts commit 32a8c0706ea2da10c29499cde976ff95398a2c6c.

--- src/utils/auth/puc.js
+++ src/utils/auth/puc.js
@@ -66,6 +66,7 @@ export function getNormalizedPucCandidatesFromSession(session) {
 
   const principalName = normalizePuc(principalNameRaw)
   const targetedId = normalizePuc(targetedIdRaw)
+  const sessionId = normalizePuc(session?.id)
 
   const candidates = []
 
@@ -75,6 +76,10 @@ export function getNormalizedPucCandidatesFromSession(session) {
 
   if (principalName) {
     candidates.push(principalName)
+  }
+
+  if (sessionId) {
+    candidates.push(sessionId)
   }
 
   return Array.from(new Set(candidates))
