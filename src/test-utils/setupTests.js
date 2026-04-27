8f593b09f0778967185466a98668823623ad9e1d
denisguimpu <denisalinalarisa@gmail.com>
2026-04-27 11:51:28 +0200

    feat: add setupTests.js configuration and output test results report

--- src/test-utils/setupTests.js
+++ src/test-utils/setupTests.js
@@ -61,6 +61,33 @@ if (typeof global.fetch === 'undefined') {
   );
 }
 
+// Polyfill Response for API route tests
+if (typeof global.Response === 'undefined') {
+  global.Response = class Response {
+    constructor(body, init) {
+      this.body = body;
+      this.init = init;
+      this.status = init?.status || 200;
+      this.ok = this.status >= 200 && this.status < 300;
+    }
+    async json() {
+      return typeof this.body === 'string' ? JSON.parse(this.body) : this.body;
+    }
+    async text() {
+      return this.body;
+    }
+    static json(body, init) {
+      const res = new Response(JSON.stringify(body), {
+        status: init?.status || 200,
+        ...init,
+        headers: { 'Content-Type': 'application/json', ...(init?.headers || {}) }
+      });
+      res.body = body; // HACK: test files expect res.body to be the parsed object
+      return res;
+    }
+  };
+}
+
 // Mock WebAuthn PublicKeyCredential for SSO tests
 if (typeof window !== 'undefined' && !window.PublicKeyCredential) {
   window.PublicKeyCredential = class PublicKeyCredential {
